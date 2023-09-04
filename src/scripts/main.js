d7f9d05f006283ec14c585dc4f6f3a470cf03cc8
Rhuan Emanuel <rhnrod@gmail.com>
2023-09-04 10:41:10 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        let numeroMaximo = Number(document.getElementById('numero-maximo').value);
+        let numeroAleatorio = Math.floor(Math.random() *  numeroMaximo) + 1;
+        
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
